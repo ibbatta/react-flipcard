78c69b20e2dd65e669e1bb7b208fd3ccace6bdd7
Maurizio Battaghini <maurizio.battaghini@gmail.com>
2017-10-31 16:32:54 +0100

    styling template topbar

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import style from './Card.scss';
+import location from '../../img/location-placeholder.svg';
+import weather from '../../img/weather-placeholder.svg';
 
 class App extends React.Component {
   constructor(props) {
@@ -21,18 +23,53 @@ class App extends React.Component {
   render() {
     return (
       <div className={style.Card}>
-        Hello
+        <div className={style.topBar}>
+          <div className={style.locationWrapper}>
+            <img className="locationIcon" src={location} alt="location-icon" />
+            <span className="locationText">Rome, Italy</span>
+          </div>
+          <div className={style.temperatureWrapper}>
+            <span className="temperatureText">
+              54°c
+            </span>
+          </div>
+        </div>
+        <div className="weatherContent">
+          <div className="weatherText">
+            Sunny
+          </div>
+          <div className="weatherIconWrapper">
+            <img className="weatherIcon" src={weather} alt="weather-icon" />
+          </div>
+        </div>
+        <div className="bottomBar">
+          <div className="humidity">
+            48% umidità
+          </div>
+          <div className="wind">
+            46 nodi
+          </div>
+        </div>
+        <div className="calendarBar">
+          <div className="calendarDay">Mon</div>
+          <div className="calendarDay">Tue</div>
+          <div className="calendarDay">Wed</div>
+          <div className="calendarDay">Thu</div>
+          <div className="calendarDay">Fri</div>
+          <div className="calendarDay">Sat</div>
+          <div className="calendarDay">Sun</div>
+        </div>
       </div>
     );
   }
 }
 
 /* App.propTypes = {
-  location: PropTypes.string,
+          location: PropTypes.string,
 };
 
 App.defaultProps = {
-  location: 'Rome, Italy',
+          location: 'Rome, Italy',
 }; */
 
 export default App;
