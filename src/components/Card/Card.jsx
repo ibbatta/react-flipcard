5992c2912bfa8594d60ca0b03313db1c5065a638
Maurizio Battaghini <maurizio.battaghini@iquii.com>
2017-11-01 00:50:08 +0100

    update style template

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import style from './Card.scss';
 import pinImg from '../../img/pin.svg';
 import celsiusImg from '../../img/weather-icon/c.svg';
+
+import img from '../../img/weather-icon/day.svg';
 
 class App extends React.Component {
   constructor(props) {
@@ -36,17 +39,17 @@ class App extends React.Component {
           </div>
         </div>
         <div className={style.weatherBar}>
+          <div className={style.weatherIconWrapper}>
+            <img className={style.weatherIcon} src={img} alt="weather-icon" />
+          </div>
           <div className={style.weatherText}>
             Sunny
-          </div>
-          <div className={style.weatherIconWrapper}>
-            <img className={style.weatherIcon} src="" alt="weather-icon" />
           </div>
         </div>
         <div className={style.bottomBar}>
           <div className={style.humidityWrapper}>
             <span className={style.humidityText}>
-              Percentuale di umidità
+              Percentuale umidità
             </span>
             <span className={style.humidityText}>
               48%
@@ -61,44 +64,44 @@ class App extends React.Component {
             </span>
           </div>
         </div>
-        {/* <div className="calendarBar">
-          <div className="calendarDay">
-            <span className="calendarText">
+        <div className={style.calendarBar}>
+          <div className={style.calendarDay}>
+            <span className={style.calendarText}>
               Mon
             </span>
           </div>
-          <div className="calendarDay">
-            <span className="calendarText">
+          <div className={style.calendarDay}>
+            <span className={style.calendarText}>
               Tue
             </span>
           </div>
-          <div className="calendarDay">
-            <span className="calendarText">
-              Wed
+          <div className={style.calendarDay}>
+            <span className={style.calendarText}>
+              Wen
             </span>
           </div>
-          <div className="calendarDay">
-            <span className="calendarText">
+          <div className={classNames(style.calendarDay, style.actual)}>
+            <span className={style.calendarText}>
               Thu
             </span>
           </div>
-          <div className="calendarDay">
-            <span className="calendarText">
+          <div className={style.calendarDay} >
+            <span className={style.calendarText}>
               Fri
             </span>
           </div>
-          <div className="calendarDay">
-            <span className="calendarText">
+          <div className={style.calendarDay}>
+            <span className={style.calendarText}>
               Sat
             </span>
           </div>
-          <div className="calendarDay">
-            <span className="calendarText">
+          <div className={style.calendarDay}>
+            <span className={style.calendarText}>
               Sun
             </span>
           </div>
-        </div> */}
-      </div>
+        </div>
+      </div >
     );
   }
 }
