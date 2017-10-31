e49f5c03b772c3d262c45a53b80cff3c49947cb1
Maurizio Battaghini <maurizio.battaghini@gmail.com>
2017-10-31 09:33:27 +0100

    initialize dev and build script with webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+};
+
+module.exports = {
+  context: path.resolve(__dirname, paths.SRC),
+  entry: {
+    app: ['./index.jsx'],
+  },
+  output: {
+    path: paths.DIST,
+    filename: '[name].bundle.js',
+  },
+  module: {},
+};
