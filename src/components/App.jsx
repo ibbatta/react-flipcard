b4214d1c791fa1aea3b6cbc86f1ddbf4c86c6a0a
Maurizio Battaghini <maurizio.battaghini@gmail.com>
2017-11-03 17:22:47 +0100

    working on map component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import classNames from 'classnames';
 import 'whatwg-fetch';
 import FrontCard from './FrontCard/FrontCard';
 import BackCard from './BackCard/BackCard';
 import style from './App.scss';
+import mapImg from '../img/map.svg';
+import weatherImg from '../img/weather.svg';
+
+let iconBtn = mapImg;
+const weatherApiKey = 'eed4cc3e34d141e2922a0ea06dc8cb4f';
+const googleMapApiKey = 'AIzaSyDMC-w0Zmbbn9fVlTsabWhZns71ePpLLeA';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isFlipped: false,
       lang: 'it',
       temperatureUnit: 'c',
       windspeedUnit: 'mph',
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -24,7 +33,7 @@ class App extends React.Component {
 
       const success = (pos) => {
         // const apiUrl = `https://api.weatherbit.io/v2.0/current?lang=it&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&key=eed4cc3e34d141e2922a0ea06dc8cb4f`;
-        const apiUrl = `https://api.weatherbit.io/v2.0/current?lang=${this.state.lang}&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&key=eed4cc3e34d141e2922a0ea06dc8cb4f`;
+        const apiUrl = `https://api.weatherbit.io/v2.0/current?lang=${this.state.lang}&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&key=${weatherApiKey}`;
         const request = new Request(apiUrl, {
           method: 'GET',
           mode: 'cors',
@@ -32,6 +41,11 @@ class App extends React.Component {
           headers: {
             'Content-Type': 'text/json; charset=utf-8',
           },
+        });
+
+        this.setState({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
         });
 
         fetch(request)
@@ -58,16 +72,41 @@ class App extends React.Component {
       const error = (err) => {
         console.error(`ERROR (${err.code}): ${err.message}`); //eslint-disable-line
       };
+
       navigator.geolocation.getCurrentPosition(success, error, options);
     } else {
       console.error('Your browser does not support geolocation'); //eslint-disable-line
     }
   }
 
+  handleClick(e) {
+    e.preventDefault();
+    this.setState(prevState => ({
+      isFlipped: !prevState.isFlipped,
+    }));
+    if (this.state.isFlipped) {
+      iconBtn = mapImg;
+    } else {
+      iconBtn = weatherImg;
+    }
+  }
+
+
   render() {
     return (
       <div className={style.AppContainer}>
-        <div className={style.AppContainer__item}>
+        <button
+          className={style.AppContainer__toggle}
+          onClick={this.handleClick}
+        >
+          <img className={style.AppContainer__img} src={iconBtn} alt="flip-icon" />
+          <span>{this.state.isFlipped ? 'Weather' : 'Map'}</span>
+        </button>
+        <div
+          className={
+            classNames(style.AppContainer__item, this.state.isFlipped ? style.flipped : null)
+          }
+        >
           <FrontCard
             temperatureUnit={this.state.temperatureUnit}
             windspeedUnit={this.state.windspeedUnit}
@@ -81,7 +120,7 @@ class App extends React.Component {
             pod={this.state.pod}
             lang={this.state.lang}
           />
-          <BackCard />
+          <BackCard lat={this.state.lat} lng={this.state.lng} apikey={googleMapApiKey} />
         </div>
       </div>
     );
--- src/components/BackCard/BackCard.jsx
+++ src/components/BackCard/BackCard.jsx
@@ -1,21 +1,59 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import style from './BackCard.scss';
-
 
 class BackCard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      lat: this.props.lat,
+      lng: this.props.lng,
+    };
+  }
+
+  componentWillReceiveProps() {
+    console.log('WILL RECEIVE PROPS');
+    console.log(`PROPS: Lat ${this.props.lat} - Lng ${this.props.lng}`);
+    this.setState({
+      lat: this.props.lat,
+      lng: this.props.lng,
+    });
+  }
+
+  componentDidUpdate() {
+    this.initMap();
+  }
+
+  initMap() {
+    console.log(`STATE: Lat ${this.state.lat} - Lng ${this.state.lng}`);
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: this.props.lat, lng: this.props.lng },
+      zoom: 8,
+    });
+    console.log(map);
   }
 
   render() {
     return (
       <div className={classNames(style.Card, style.CardBack)}>
-        <h1 className={style.prova}>Ciao</h1>
+        <div id="map" className={style.CardBack__MapContainer} />
+        <script async defer src={`https://maps.googleapis.com/maps/api/js?sensor=false&key=${this.props.apiKey}callback=${this.initMap}`} />
       </div>
     );
   }
 }
+
+BackCard.propTypes = {
+  lat: PropTypes.number,
+  lng: PropTypes.number,
+  apiKey: PropTypes.string,
+};
+
+BackCard.defaultProps = {
+  lat: 0,
+  lng: 0,
+  apiKey: '',
+};
 
 export default BackCard;
